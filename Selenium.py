71ad38e457634d096cd8e1a6d9be9aa8c2ae1904
NicaRbt <Robert.nica94@gmail.com>
2025-03-19 19:43:42 +0200

    Create Selenium.py
    First selenium draft

---
+++ Selenium.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys as keys
+from assertpy import soft_assertions, assert_that
+import pytest
+
+driver = webdriver.Chrome()
+driver.get("https://www.emag.ro")
+#driver.maximize_window()
+
+#Locatori
+search_bar = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "searchboxTrigger"))
+)
+
+search_bar.send_keys("laptop")
+search_bar.send_keys(keys.ENTER)
+
+file_result = driver.find_element(By.CLASS_NAME, "card-v2")
+file_result.click()
+title = driver.title
+print(title)
+
+
+with soft_assertions():
+    assert_that(title).contains("Laptop")  # Titlul nu va conține "xxx", deci testul va eșua
+
+driver.quit()
+
+
+#help_button = driver.find_element(By.CLASS_NAME, "navbar-aux-help-link")
+#Oferta_zilei = driver.find_element(By.LINK_TEXT, "Oferta Zilei")
+#Logo = driver.find_element(By.XPATH, '//*[@id="masthead"]/div/div/div[1]/a/img')
+#search_bar.send_keys("laptop")
