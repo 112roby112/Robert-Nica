cbde846da642611c3e57d22906ffa41a0b489bc8
NicaRbt <Robert.nica94@gmail.com>
2025-03-28 09:55:14 +0200

    up

--- python.py
+++ python.py
@@ -7,22 +7,22 @@ class Elev:
         self.istorie = istorie
 
     def calculeaza_media(self):
-        # Calculăm media aritmetică a notelor
+        
         return (self.mate + self.romana + self.biologie + self.istorie) / 4
 
     def afiseaza_media(self):
-        # Afișăm numele elevului și media
+        
         print(f"Elevul {self.nume} are media: {self.calculeaza_media():.2f}")
 
-# Exemplu de utilizare
+
 nume_elev = input("Introdu numele elevului: ")
 mate = float(input("Introdu nota la matematică: "))
 romana = float(input("Introdu nota la limba română: "))
 biologie = float(input("Introdu nota la biologie: "))
 istorie = float(input("Introdu nota la istorie: "))
 
-# Creăm un obiect de tip Elev
+
 elev = Elev(nume_elev, mate, romana, biologie, istorie)
 
-# Afișăm media elevului
+
 elev.afiseaza_media()
